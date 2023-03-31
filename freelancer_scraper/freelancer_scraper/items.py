cfdcc149876058950ca81e8f4983407bee7f0fef
Aryan Hamedani <aryn.hmd@gmail.com>
2023-03-31 14:21:50 +0330

    JobItem was defined

--- freelancer_scraper/freelancer_scraper/items.py
+++ freelancer_scraper/freelancer_scraper/items.py
@@ -6,7 +6,16 @@
 import scrapy
 
 
-class FreelancerScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class JobItem(scrapy.Item):
+    title = scrapy.Field()
+    url = scrapy.Field()
+    status = scrapy.Field()
+    budget = scrapy.Field()
+    description = scrapy.Field()
+    skills = scrapy.Field()
+    client = scrapy.Field()
+    project_id = scrapy.Field()
+    days_left = scrapy.Field()
+    bids = scrapy.Field()
+    customer_other_jobs = scrapy.Field()
+    similar_jobs = scrapy.Field()
