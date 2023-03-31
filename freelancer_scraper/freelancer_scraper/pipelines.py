dc2311a37c938a418e99a5dfcfa3d7efe7aa6fbf
Aryan Hamedani <aryn.hmd@gmail.com>
2023-03-31 14:55:04 +0330

    JobItemPipeline implemented

--- freelancer_scraper/freelancer_scraper/pipelines.py
+++ freelancer_scraper/freelancer_scraper/pipelines.py
@@ -6,8 +6,35 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
 
 
-class FreelancerScraperPipeline:
+class JobItemPipeline:
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get("MONGO_URI"),
+            mongo_db=crawler.settings.get("MONGO_DATABASE"),
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        
+        existing_item = self.db.jobs.find_one({'url': adapter['url']})
+        if existing_item:
+            spider.logger.info(f"Item already exists: {adapter['url']}")
+            return item
+
+        self.db.jobs.insert_one(adapter.asdict())
         return item
